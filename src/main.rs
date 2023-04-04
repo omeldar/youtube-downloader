614072f835fae855a74d97a20cd137e0da685a78
Eldar Omerovic <eldar.work.edu@gmail.com>
2023-04-04 23:02:48 +0200

    Using rusty_ytdl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use text_io::read;
+use rusty_ytdl::*;
+
+#[tokio::main]
+async fn main() {
+    println!("Enter your youtube video url");
+    let url: String = read!();
+    println!("{}", url);
+
+    let video_options = VideoOptions {
+        quality: VideoQuality::Highest,
+        filter: VideoSearchOptions::VideoAuido,
+        ..Default::default()
+    };
+
+    let video = Video::new_with_options(url, video_options).unwrap();
+    _ = video.download().await;
 }
